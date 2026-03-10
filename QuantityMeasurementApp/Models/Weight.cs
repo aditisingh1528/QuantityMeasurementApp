4ebb9646e1c87450766f9cb00f0b7c167e7c61c2
aditisingh1528 <aditisingh1528@gmail.com>
2026-03-10 16:30:28 +0530

    [Aditi] Add. Implemented UC13- Centralized Arithmetic Logic to Enfore DRY in Quantity Operations

---
+++ QuantityMeasurementApp/Models/Weight.cs
@@ -0,0 +1,97 @@
+using System;
+
+namespace QuantityMeasurementApp.Models
+{
+    /// <summary>
+    /// Represents a weight measurement.
+    /// Immutable value object.
+    /// </summary>
+    public sealed class Weight
+    {
+        private const double Epsilon = 1e-6;
+
+        public double Value { get; }
+        public WeightUnit Unit { get; }
+
+        public Weight(double value, WeightUnit unit)
+        {
+
+            if (double.IsNaN(value) || double.IsInfinity(value))
+                throw new ArgumentException("Invalid numeric value.");
+
+            Value = value;
+            Unit = unit;
+        }
+
+        // Equality
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj is null || obj.GetType() != typeof(Weight))
+                return false;
+
+            var other = (Weight)obj;
+
+            double thisBase = Unit.ConvertToBaseUnit(Value);
+            double otherBase = other.Unit.ConvertToBaseUnit(other.Value);
+
+            return Math.Abs(thisBase - otherBase) <= 0.01;
+        }
+
+        public override int GetHashCode()
+        {
+            double baseValue = Unit.ConvertToBaseUnit(Value);
+            return baseValue.GetHashCode();
+        }
+
+        // Conversion
+        
+        public Weight ConvertTo(WeightUnit targetUnit)
+        {
+
+            double baseValue = Unit.ConvertToBaseUnit(Value);
+            double converted = targetUnit.ConvertFromBaseUnit(baseValue);
+
+            return new Weight(converted, targetUnit);
+        }
+
+        // Addition (implicit target)
+        
+        public Weight Add(Weight other)
+        {
+            if (other == null)
+                throw new ArgumentException("Other weight cannot be null.");
+
+            double baseSum =
+                Unit.ConvertToBaseUnit(Value) +
+                other.Unit.ConvertToBaseUnit(other.Value);
+
+            double result = Unit.ConvertFromBaseUnit(baseSum);
+
+            return new Weight(result, Unit);
+        }
+
+        // Addition (explicit target)
+        
+        public Weight Add(Weight other, WeightUnit targetUnit)
+        {
+            if (other == null)
+                throw new ArgumentException("Other weight cannot be null.");
+
+            double baseSum =
+                Unit.ConvertToBaseUnit(Value) +
+                other.Unit.ConvertToBaseUnit(other.Value);
+
+            double result = targetUnit.ConvertFromBaseUnit(baseSum);
+
+            return new Weight(result, targetUnit);
+        }
+
+        public override string ToString()
+        {
+            return $"{Math.Round(Value, 6)} {Unit}";
+        }
+    }
+}
